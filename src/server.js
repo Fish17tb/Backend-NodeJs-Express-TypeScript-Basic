d5c43258d03560c5f319ab4491710b520455c12a
Nguyên Vũ <fish17tb@gmail.com>
2025-03-30 14:49:36 +0700

    Connect-With-Database-Pass-Mongodb-Driver

--- src/server.js
+++ src/server.js
@@ -4,8 +4,9 @@ const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const apiRoutes = require("./routes/api");
 const connection = require("./config/database");
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 // const Kitten = require("./models/Kitten");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 8386;
@@ -26,7 +27,6 @@ configViewEngine(app);
 app.use("/", webRoutes);
 app.use("/v1/api/", apiRoutes);
 
-
 // // create new a document
 // const cat = new Kitten({ name: "new user" });
 // console.log("my-document:", cat.name); // 'cat'
@@ -35,8 +35,23 @@ app.use("/v1/api/", apiRoutes);
 // anonymous function to handle logic run order
 (async () => {
   try {
+    // using mongoose
     // test connection
-    await connection();
+    // await connection();
+
+    // using mongodb
+    // Connection URL
+    const url = process.env.DB_HOST_WITH_DRIVER;
+    const client = new MongoClient(url);
+
+    // Database Name
+    const dbName = process.env.DB_NAME;
+    await client.connect();
+    console.log("Connected successfully to server");
+    
+    const db = client.db(dbName);
+    const collection = db.collection("documents");
+
     // listen
     app.listen(port, hostname, () => {
       console.log(`Backend app listening on port ${port}`);
