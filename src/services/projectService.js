a27c008ef1545a0b2a9921b5110cdd7b53c5339a
Nguyên Vũ <fish17tb@gmail.com>
2025-04-07 16:09:38 +0700

    Add-Users-On-Projects
    add one or arrUsers on table projects pass Id user (usersArr in Postman)

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -6,6 +6,16 @@ const CreateProjectService = async (data) => {
       let result = await Project.create(data);
       return result;
     }
+    if (data.type === "ADD-USERS") {
+      // console.log("hnv-testData", data)
+      let myProject = await Project.findById(data.projectId);
+      // console.log("hnv-projectId", myProject);
+      for (let i = 0; i < data.usersArr.length; i++) {
+        myProject.usersInfor.push(data.usersArr[i]);
+      }
+      let newResult = await myProject.save();
+      return newResult;
+    }
   } catch (error) {
     console.log("ERROR", error);
     return null;
